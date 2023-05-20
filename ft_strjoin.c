44d66b177ae894d6a2d4ab1e936359c2f2d82906
Tsubasa Ishikawa <tsishika@c1r26s9.42tokyo.jp>
2023-05-20 12:48:21 +0900

    fix ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsishika <syi378039@gmail.com>             +#+  +:+       +#+        */
+/*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 00:11:06 by tsishika          #+#    #+#             */
-/*   Updated: 2023/05/20 00:40:44 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/05/20 12:45:36 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	len;
+	char	*p;
+	size_t	i;
 
-	if (!s1 || !s2)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = (char *) malloc(len);
-	if (!str)
+	i = ft_strlen(s1) + ft_strlen(s2) + 1;
+	p = malloc(sizeof(char) * i);
+	if (p == NULL)
 		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	ft_strlcat(str, s2, len);
-	return (str);
+	ft_strlcpy(p, s1, ft_strlen(s1) + 1);
+	ft_strlcat(p, s2, i);
+	return (p);
 }
