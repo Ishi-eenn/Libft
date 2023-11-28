d834bf01c3f04c62b7527c99cd999dc0dcd0530a
Ishi <syi378039@gmail.com>
2023-11-28 09:24:02 +0900

    fix

--- ctype/ft_isalnum.c
+++ ctype/ft_isalnum.c
@@ -6,11 +6,11 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:00:56 by tsishika          #+#    #+#             */
-/*   Updated: 2023/11/27 11:39:11 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/11/28 09:20:55 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_ctype.h"
 
 int	ft_isalnum(int c)
 {
--- list/ft_lstadd_front.c
+++ list/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 22:04:14 by tsishika          #+#    #+#             */
-/*   Updated: 2023/11/23 20:55:19 by tsishika         ###   ########.fr       */
+/*   Updated: 2023/11/28 09:21:31 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,3 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
-
-// #include <stdio.h>
-
-// int	main(void)
-// {
-// 	t_list *list = ft_lstnew("1");
-// 	ft_lstadd_front(&list, ft_lstnew("2"));
-// 	printf("%s\n", (char *)lst->content);
-// 	printf("%s\n", (char *)lst->next->content);
-// }
